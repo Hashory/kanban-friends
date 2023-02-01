947a1126eff67383da6f64d6f245c167a45c7230
coREbono <89839285+Hashory@users.noreply.github.com>
2023-02-01 11:09:25 +0900

    change api server url

--- src/lib/apiserver.js
+++ src/lib/apiserver.js
@@ -2,5 +2,5 @@ export default function APIserverURl() {
   if (process.env.NODE_ENV === "development") {
     return "http://localhost:3000/v1/";
   }
-  return "https://api.example.com/v1/";
+  return "https://kanban-friends-server.onrender.com/v1/";
 }
